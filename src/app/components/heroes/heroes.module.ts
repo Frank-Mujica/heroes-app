dc555f87ddc0d4f34a930451863c8c00301f9224
frank.mujica <frank.mujica@siigroup.cl>
2020-11-17 15:03:38 -0300

    Bold Pipe

--- src/app/components/heroes/heroes.module.ts
+++ src/app/components/heroes/heroes.module.ts
@@ -4,25 +4,25 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { HeroProfileComponent } from './hero-profile/hero-profile.component';
 import { ListadoDeHeroesComponent } from './listado-de-heroes/listado-de-heroes.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CapitalizePipe } from 'src/app/pipes/capitalize.pipe';
 import { RouterModule } from '@angular/router';
+import { BoldPipe } from 'src/app/pipes/bold.pipe';
 
 @NgModule({
   declarations: [
     HeroProfileComponent,
     ListadoDeHeroesComponent,
     CapitalizePipe,
+    BoldPipe
   ],
   imports: [
     CommonModule,
     RouterModule,
     SharedModule,
     HeroesRoutingModule,
-    // BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
